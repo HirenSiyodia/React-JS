41989c07e72689e7fc85aa8f93d77f87d668b28f
Hiren Siyodia | Front End Developer <siyodiahiren7@gmail.com>
2025-06-23 17:40:36 +0000

    onClick Alert

---
+++ my-app/src/Components/onClick.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+
+    const onClick = () => {
+        alert("Helo Hiren");
+    }
+    return(
+        <button onClick={onClick}>Submit me
+        </button>
+    );
+}
+
+export default App;
+
+// This is for a aler calling in a same components;
